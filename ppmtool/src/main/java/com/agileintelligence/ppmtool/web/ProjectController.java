fbece03cff290e9dc6c5f8409cd62fe6b679e4b9
Shashank201 <svarshney17@gmail.com>
2020-10-01 10:30:13 +0530

    refactor project controller

--- ppmtool/src/main/java/com/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/agileintelligence/ppmtool/web/ProjectController.java
@@ -1,22 +1,18 @@
 package com.agileintelligence.ppmtool.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.agileintelligence.ppmtool.domain.Project;
+import com.agileintelligence.ppmtool.services.MapValidateErrorService;
 import com.agileintelligence.ppmtool.services.ProjectService;
 
 @RestController
@@ -24,18 +20,17 @@ import com.agileintelligence.ppmtool.services.ProjectService;
 public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
+	@Autowired
+	private MapValidateErrorService mapValidationErrorService;
 
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
 
-		if (bindingResult.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-			for (FieldError error : bindingResult.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidateError(bindingResult);
 
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		if (errorMap != null)
+			return errorMap;
+
 		return new ResponseEntity<Project>(projectService.saveOrUpdateProject(project), HttpStatus.CREATED);
 	}
 }
