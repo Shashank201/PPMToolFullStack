4cc839d609ae999e8e315736be4ba4f6b2a0b61a
Shashank201 <svarshney17@gmail.com>
2020-12-09 20:00:35 +0530

    Get projects completed

---
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -0,0 +1,18 @@
+import { GET_PROJECT } from "../actions/types";
+
+const initialState = {
+  projects: [],
+  project: {},
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_PROJECT:
+      return {
+        ...state,
+        projects: action.payload,
+      };
+    default:
+      return state;
+  }
+}
